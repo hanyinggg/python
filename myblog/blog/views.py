09fd4262dfcbbadba5f5e183bb9bb1df51a84b67
hanyinggg <15872402489@163.com>
2018-05-11 20:59:27 +0800

    comiit

---
+++ myblog/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Post
+
+def index(request):
+	post_list = Post.objects.all().order_by('-created_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/detail.html', context ={'post': post})
+
+
+
+
+
+
+
+# Create your views here.
